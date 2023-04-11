aff4165e1ccc44315fa434b65d63a374b36f372e
Paulina <pjlobocka@gmail.com>
2023-04-11 23:13:15 +0200

    astar zoptymalizowany d)

--- src/Main.java
+++ src/Main.java
@@ -3,29 +3,34 @@ import java.time.LocalTime;
 public class Main {
     public static void main(String[] args) throws Exception {
         var graph = DataLoader.getGraph();
-        //graph.printGraph();
-        System.out.println("Dijkstra result:");
-        var result1 = Dijkstra.shortestPath(graph, "Magellana", "Rogowska", LocalTime.of(9, 17, 0));
-        System.out.println("A* time criteria result:");
-        Astar.astarTimeCriteria(graph, "Magellana", "Rogowska", LocalTime.of(9, 17, 0));
-        System.out.println("A* changes criteria result:");
-        Astar.astarChangesCriteria(graph, "Magellana", "Rogowska", LocalTime.of(9, 17, 0));
 
 //        System.out.println("Dijkstra result:");
-//        var result2 = Dijkstra.shortestPath(graph, "Wielka", "Klimasa", LocalTime.of(10, 45, 0));
-//        System.out.println("A* result:");
-//        Astar.astarTimeCriteria(graph, "Wielka", "Klimasa", LocalTime.of(10, 45, 0));
-//        System.out.println("A* changes criteria result:");
-//        Astar.astarChangesCriteria(graph, "Wielka", "Klimasa", LocalTime.of(10, 45, 0));
-
-
-//        System.out.println("Dijkstra result:");
-//        var result3 = Dijkstra.shortestPath(graph, "BISKUPIN", "Renoma", LocalTime.of(15, 00, 0));
+//        var result1 = Dijkstra.shortestPath(graph, "Magellana", "Rogowska", LocalTime.of(9, 17, 0));
 //        System.out.println("A* time criteria result:");
-//        Astar.astarTimeCriteria(graph, "BISKUPIN", "Renoma", LocalTime.of(15, 00, 0));
+//        Astar.astarTimeCriteria(graph, "Magellana", "Rogowska", LocalTime.of(9, 17, 0));
 //        System.out.println("A* changes criteria result:");
-//        Astar.astarChangesCriteria(graph, "BISKUPIN", "Renoma", LocalTime.of(15, 00, 0));
+//        Astar.astarChangesCriteria(graph, "Magellana", "Rogowska", LocalTime.of(9, 17, 0));
+//        System.out.println("A* time criteria optimized result:");
+//        Astar.astarChangesCriteriaOptimized(graph, "Magellana", "Rogowska", LocalTime.of(9, 17, 0));
 
+//        System.out.println("Dijkstra result:");
+//        var result2 = Dijkstra.shortestPath(graph, "Wielka", "KLECINA", LocalTime.of(10, 45, 0));
+//        System.out.println("A* result:");
+//        Astar.astarTimeCriteria(graph, "Wielka", "KLECINA", LocalTime.of(10, 45, 0));
+//        System.out.println("A* changes criteria result:");
+//        Astar.astarChangesCriteria(graph, "Wielka", "KLECINA", LocalTime.of(10, 45, 0));
+//        System.out.println("A* time criteria optimized result:");
+//        Astar.astarChangesCriteriaOptimized(graph, "Wielka", "KLECINA", LocalTime.of(10, 45, 0));
+//
+        System.out.println("Dijkstra result:");
+        var result3 = Dijkstra.shortestPath(graph, "Godebskiego (AWF Wrocław)", "PORT LOTNICZY", LocalTime.of(15, 00, 0));
+        System.out.println("A* time criteria result:");
+        Astar.astarTimeCriteria(graph, "Godebskiego (AWF Wrocław)", "PORT LOTNICZY", LocalTime.of(15, 00, 0));
+        System.out.println("A* changes criteria result:");
+        Astar.astarChangesCriteria(graph, "Godebskiego (AWF Wrocław)", "PORT LOTNICZY", LocalTime.of(15, 00, 0));
+        System.out.println("A* time criteria optimized result:");
+        Astar.astarChangesCriteriaOptimized(graph, "Godebskiego (AWF Wrocław)", "PORT LOTNICZY", LocalTime.of(15, 00, 0));
+//
 
     }
 }
